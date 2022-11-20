454947160dd0f24e2931587fc0488ae4f5e2b5a3
lu-tar <l.tar@protonmail.com>
2022-11-20 23:05:04 +0100

    test

--- shell_app/testing.py
+++ shell_app/testing.py
@@ -1,15 +1,9 @@
-mask = 24
-bin_train = [[00000000],[00000000],[0000000],[00000000]]
-bin_train = ["0000000000000000000000000000000"]
+bin_train = "00000000000000000000000000000000"
+mask = 15
+bin_train = bin_train.replace("0","1",mask)
+first_oct = int(bin_train[0:8],2)
+second_oct = int(bin_train[8:16],2)
+third_oct = int(bin_train[16:24],2)
+fourth_oct = int(bin_train[24:32],2)
 
-i = 0
-while i < len(bin_train):
-    decimale = 0
-    potenza = 1
-    while bin_train[i][0]>0:
-        resto = bin_train[i][0]%10
-        bin_train[i][0] = bin_train[i][0]//10
-        decimale += resto*potenza
-        potenza = potenza*2
-    print(decimale)
-    i = i + 1
+print ("%s.%s.%s.%s" % (first_oct, second_oct, third_oct, fourth_oct))
