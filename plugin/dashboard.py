f2d95bbdc2b21403c6efc170b8e889ad20c42036
l1ca7193 <luca97.tarozzi@gmail.com>
2023-12-01 16:52:04 +0100

    Testing textual TUI

--- plugin/dashboard.py
+++ plugin/dashboard.py
@@ -1,3 +1,26 @@
-from textual.app import App
+from textual.app import App, ComposeResult, RenderResult
+from textual.containers import Horizontal, Vertical
+from textual.widget import Widget
+from textual.widgets import Static
 
-App.run()
+class Hello(Widget):
+    """Display a greeting."""
+    BORDER_TITLE = "Hello Widget"
+    def render(self) -> RenderResult:
+        return "Hello, [b]World[/b]!"
+
+class UtilityContainersExample(App):
+    CSS_PATH = "utility_containers.tcss"
+    def compose(self) -> ComposeResult:
+        with Horizontal():
+            with Vertical(classes="column"):
+                yield Static("One")
+                yield Static("Two")
+            with Vertical(classes="column"):
+                yield Static("Three")
+                yield Static("Four")
+
+
+if __name__ == "__main__":
+    app = UtilityContainersExample()
+    app.run()
