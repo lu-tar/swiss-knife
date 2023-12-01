66cbfaea0f786f6d83c9011f050e8ae4d69a6dbb
l1ca7193 <luca97.tarozzi@gmail.com>
2023-12-01 17:52:13 +0100

    Hello World with textual

--- plugin/dashboard.py
+++ plugin/dashboard.py
@@ -1,22 +1,35 @@
-from textual.app import App, ComposeResult
+from textual.app import App, ComposeResult, RenderResult
 from textual.containers import Container, Horizontal, VerticalScroll
 from textual.widgets import Header, Static
+from textual.widget import Widget
+
+class Hello(Widget):
+    """Display a greeting."""
+
+    def render(self) -> RenderResult:
+        return ("Hi\n")
 
 # Test layout + widget
 class CombiningLayoutsExample(App):
     CSS_PATH = "combining_layouts.tcss"
 
+    # on_mount = all'avvio
+    def on_mount(self) -> None:
+        self.title = "Test bench"
+        self.sub_title = "and sub-title"
+
     def compose(self) -> ComposeResult:
-        yield Header()
+        yield Header(show_clock=True)
         with Container(id="app-grid"):
-            with VerticalScroll(id="left-pane"):
-                for number in range(15):
-                    yield Static(f"Vertical layout, child {number}")
+            with Static(id="left-pane"):
+                yield Hello()
+            
             with Horizontal(id="top-right"):
                 yield Static("Horizontally")
                 yield Static("Positioned")
                 yield Static("Children")
                 yield Static("Here")
+            
             with Container(id="bottom-right"):
                 yield Static("This")
                 yield Static("panel")
