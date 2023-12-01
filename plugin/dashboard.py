bcabd3f389e322991fa510e2f4a722e1aacf8d0f
l1ca7193 <luca97.tarozzi@gmail.com>
2023-12-01 16:53:42 +0100

    Base per testing widget

--- plugin/dashboard.py
+++ plugin/dashboard.py
@@ -3,12 +3,14 @@ from textual.containers import Horizontal, Vertical
 from textual.widget import Widget
 from textual.widgets import Static
 
+# Primo widget
 class Hello(Widget):
     """Display a greeting."""
     BORDER_TITLE = "Hello Widget"
     def render(self) -> RenderResult:
         return "Hello, [b]World[/b]!"
 
+# Classe di base
 class UtilityContainersExample(App):
     CSS_PATH = "utility_containers.tcss"
     def compose(self) -> ComposeResult:
